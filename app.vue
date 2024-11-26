28d90c4909fec1cb14b6d94a1471f03a565d0368
JamesLambert1986 <jameslambert1986@gmail.com>
2024-11-26 22:09:11 +0000

    Add a maintenance page

--- app.vue
+++ app.vue
@@ -1,13 +1,22 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <Header />
-  <NuxtLayout>
+  <div class="invert-colours bg-info gradient-primary gradient-direction-angle" v-if="true">
+    <div class="container pt-5 pb-5 min-vh-100 d-md-flex">
+      <div class="row align-items-center">
+        <div class="col-12 col-md-6">
+          <h1>This page is down for maintenance</h1>
+          <p>We're busy updating the design site. We'll be back up and running again shortly.</p>
+          <p>In the meantime, please take this opportunity to visit our iamkey design system.</p>
+          <span class="reset-colours"><a href="https://iamproperty.github.io/" class="btn btn-primary">Design System</a></span>
+        </div>
+        <div class="col-8 col-sm-10 col-md-5 ms-auto me-auto">
+          <img class="d-block ms-auto me-auto" src="./assets/images/illustrations/contractors.png" />
+      </div>
+      </div>
+    </div>
+  </div>
+  <NuxtLayout v-else>
     <NuxtPage />
   </NuxtLayout>
-  <Footer />
 </template>
 
 <style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,5 @@
 <template>
+  <Header />
   <slot />
+  <Footer />
 </template>
