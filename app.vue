b7e3b181b686138cea5b21a8c61bbbabe446ba94
JamesLambert1986 <jameslambert1986@gmail.com>
2024-11-27 10:19:21 +0000

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <div class="container pt-5 pb-5 min-vh-100 d-md-flex">
       <div class="row align-items-center">
         <div class="col-12 col-md-6">
-          <h1>This page is down for maintenance</h1>
+          <h1>This site is down for maintenance</h1>
           <p>We're busy updating the design site. We'll be back up and running again shortly.</p>
           <p>In the meantime, please take this opportunity to visit our iamkey design system.</p>
           <span class="reset-colours"><a href="https://iamproperty.github.io/" class="btn btn-primary">Design System</a></span>
